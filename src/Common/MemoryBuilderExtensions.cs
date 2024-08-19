e09ef4e325d4bf875d77c956dd61b9c4aeb2f68b
Hans <shaomenghao@hotmail.com>
2024-08-19 16:10:13 +0800

    ollama with SemanticMemory

---
+++ src/Common/MemoryBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace Microsoft.SemanticKernel.Memory;
+
+public static class MemoryBuilderExtensions
+{
+#pragma warning disable SKEXP0001 
+    public static MemoryBuilder WithOllamaTextEmbeddingGeneration(this MemoryBuilder memoryBuilder, string modelId)
+#pragma warning restore SKEXP0001 
+    {
+        #pragma warning disable SKEXP0010
+        memoryBuilder.WithOpenAITextEmbeddingGeneration(modelId, "useless", httpClient: new HttpClient(new OllamaHttpClientHandler()));
+        return memoryBuilder;
+    }
+}
