3bbb319b069a5dce07340b323bd810d83ea41bda
Hans <shaomenghao@hotmail.com>
2024-08-21 21:13:04 +0800

    update

--- src/S05.FunctionCall/Program.cs
+++ src/S05.FunctionCall/Program.cs
@@ -5,10 +5,6 @@ using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 using S05.FunctionCall;
 
-using System;
-using System.Runtime.Serialization;
-using System.Text;
-
 #pragma warning disable SKEXP0001 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
 
 var kernel = Kernel.CreateBuilder()
@@ -26,7 +22,6 @@ var executionSettings = new OpenAIPromptExecutionSettings
 
 var chatService = kernel.GetRequiredService<IChatCompletionService>();
 var chatHistory = new ChatHistory("你是一位资深.Net开发人员，精通各种.Net框架");
-//var content = new StringBuilder();
 
 while (true)
 {
@@ -34,7 +29,6 @@ while (true)
     Console.Write("You:");
     chatHistory.AddUserMessage(Console.ReadLine()!);
 
-    //content.Clear();
     Console.ResetColor();
     Console.Write("Assistant:");
 
@@ -42,21 +36,19 @@ while (true)
     if (result.Content is not null)
     {
         Console.Write(result.Content);
-        //content.Append(result.Content);
         chatHistory.Add(result);
     }
     else
     {
         var functionCalls = FunctionCallContent.GetFunctionCalls(result);
-        if (!functionCalls.Any())
+        if (functionCalls.Any())
         {
-            break;
-        }
-
-        foreach (var functionCall in functionCalls)
-        {
-            var resultContent = await functionCall.InvokeAsync(kernel);
-            chatHistory.Add(resultContent.ToChatMessage());
+            foreach (var functionCall in functionCalls)
+            {
+                var functionContent = await functionCall.InvokeAsync(kernel);
+                Console.Write(functionContent.InnerContent);
+                chatHistory.Add(functionContent.ToChatMessage());
+            }
         }
     }
     
