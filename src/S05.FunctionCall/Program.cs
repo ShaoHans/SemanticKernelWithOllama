00d9a2d31614da3a04014d1793b9ef7c1194f599
Hans <shaomenghao@hotmail.com>
2024-08-21 20:34:07 +0800

    update

--- src/S05.FunctionCall/Program.cs
+++ src/S05.FunctionCall/Program.cs
@@ -5,7 +5,10 @@ using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 using S05.FunctionCall;
 
+using System.Runtime.Serialization;
 using System.Text;
+
+#pragma warning disable SKEXP0001 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
 
 var kernel = Kernel.CreateBuilder()
                 .AddOllamaChatCompletion("llama3.1")
@@ -16,10 +19,8 @@ kernel.Plugins.AddFromType<TimePlugin>();
 var executionSettings = new OpenAIPromptExecutionSettings
 {
     Temperature = 0.1,
-    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+    ToolCallBehavior = ToolCallBehavior.EnableKernelFunctions
 };
-
-Console.WriteLine(await kernel.InvokePromptAsync("现在几点钟，请用中文回答?", new(executionSettings)));
 
 
 var chatService = kernel.GetRequiredService<IChatCompletionService>();
@@ -35,10 +36,34 @@ while (true)
     content.Clear();
     Console.ResetColor();
     Console.Write("Assistant:");
-    await foreach (var message in chatService.GetStreamingChatMessageContentsAsync(chatHistory, executionSettings, kernel))
+
+    AuthorRole? authorRole = null;
+    var fccBuilder = new FunctionCallContentBuilder();
+
+    var result = await chatService.GetChatMessageContentAsync(chatHistory, executionSettings, kernel);
+    if(result != null)
     {
-        Console.Write(message);
-        content.Append(message.Content);
+        Console.Write(result);
+        content.Append(result.Content);
+        authorRole ??= result.Role;
+    }
+
+    var functionCalls = fccBuilder.Build();
+    if (functionCalls.Any())
+    {
+        var fcContent = new ChatMessageContent(role: authorRole ?? default, content: null);
+        chatHistory.Add(fcContent);
+
+        // Iterating over the requested function calls and invoking them
+        foreach (var functionCall in functionCalls)
+        {
+            fcContent.Items.Add(functionCall);
+
+            var functionResult = await functionCall.InvokeAsync(kernel);
+
+            chatHistory.Add(functionResult.ToChatMessage());
+            Console.Write(functionResult.ToChatMessage());
+        }
     }
 
     Console.WriteLine();
